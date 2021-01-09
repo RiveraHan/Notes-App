6202cce623bdd1ff9e2d8b10fc04ced28f4e2273
Hanzell Rivera <hanzellrivera95@gmail.com>
2021-01-08 19:33:13 -0600

    Agregamos restricciones a rutas de usuarios no logeados

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const Note = require('../models/Note');
+const { isAuthenticated } = require('../helpers/auth');
 
-router.get('/notes/add', (req, res) => {
+
+router.get('/notes/add', isAuthenticated, (req, res) => {
     res.render('notes/new-note');
 });
 
-router.post('/notes/new-note', async(req, res) => {
+router.post('/notes/new-note', isAuthenticated, async(req, res) => {
     const { title, description } = req.body;
     const errors = [];
     if (!title) {
@@ -30,7 +32,7 @@ router.post('/notes/new-note', async(req, res) => {
 
 });
 
-router.get('/notes', async(req, res) => {
+router.get('/notes', isAuthenticated, async(req, res) => {
 
     const notes = await Note.find().sort({ date: 'desc' });
     const context = {
@@ -43,7 +45,7 @@ router.get('/notes', async(req, res) => {
     res.render('notes/all-notes', { notes: context.allNotes });
 });
 
-router.get('/notes/edit/:id', async(req, res) => {
+router.get('/notes/edit/:id', isAuthenticated, async(req, res) => {
     const note = await Note.findById(req.params.id);
 
     const context = {
@@ -53,7 +55,7 @@ router.get('/notes/edit/:id', async(req, res) => {
     res.render('notes/edit-note', { note: context.noteInfo });
 });
 
-router.put('/notes/edit-note/:id', async(req, res) => {
+router.put('/notes/edit-note/:id', isAuthenticated, async(req, res) => {
     const { title, description } = req.body;
     await Note.findByIdAndUpdate(req.params.id, { title, description });
     req.flash('success_msg', 'Note Updated Successfully');
@@ -61,7 +63,7 @@ router.put('/notes/edit-note/:id', async(req, res) => {
     res.redirect('/notes');
 });
 
-router.delete('/notes/delete/:id', async(req, res) => {
+router.delete('/notes/delete/:id', isAuthenticated, async(req, res) => {
     await Note.findByIdAndDelete(req.params.id);
     req.flash('success_msg', 'Note Deleted Successfully');
 
