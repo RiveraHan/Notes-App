0fbe2cbeda7dffaee7a3c23cf0d24d8109bfe4d4
Hanzell Rivera <hanzellrivera95@gmail.com>
2021-01-18 21:45:22 -0600

    Restructura del proyecto: Notas por usuario

--- src/controllers/notes.controller.js
+++ src/controllers/notes.controller.js
@@ -20,6 +20,7 @@ const notesCtrl = {
             res.render('notes/new-note', { errors, title, description });
         } else {
             const newNote = new Note({ title, description });
+            newNote.user = req.user.id;
             await newNote.save();
             req.flash('success_msg', 'Note Added Successfully');
             res.redirect('/notes');
@@ -28,13 +29,18 @@ const notesCtrl = {
     },
     renderNote: async(req, res) => {
 
-        const notes = await Note.find({}).sort({ date: 'desc' });
+        const notes = await Note.find({ user: req.user.id }).sort({ createdAt: 'desc' });
 
         res.render('notes/all-notes', { notes });
     },
     renderEditForm: async(req, res) => {
 
         const note = await Note.findById(req.params.id);
+
+        if (note.user != req.user.id) {
+            req.flash('error_msg', 'Not Authorized');
+            return res.redirect('/notes');
+        }
 
         res.render('notes/edit-note', { note });
     },
--- src/models/Note.js
+++ src/models/Note.js
@@ -3,7 +3,7 @@ const { Schema, model } = require('mongoose');
 const NoteSchema = new Schema({
     title: { type: String, required: true },
     description: { type: String, required: true },
-    user: { type: String }
+    user: { type: Schema.Types.ObjectId, ref: 'User', required: true }
 }, {
     timestamps: true
 });
