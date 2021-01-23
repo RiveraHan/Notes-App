9edd36230e0eef126195a6c0850b2b5fcdc50356
Hanzell Rivera <hanzellrivera95@gmail.com>
2021-01-22 19:55:34 -0600

    Agredando la variable NOTES_APP_MONGODB_URI

--- src/database.js
+++ src/database.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
 const { NOTES_APP_MONGODB_HOST, NOTES_APP_MONGODB_DATBASE } = process.env;
 
-const MONGODB_URI = `mongodb://${NOTES_APP_MONGODB_HOST}/${NOTES_APP_MONGODB_DATBASE}`;
 
-mongoose.connect(MONGODB_URI, {
+process.env.NOTES_APP_MONGODB_URI = process.env.NOTES_APP_MONGODB_URI || `mongodb://${NOTES_APP_MONGODB_HOST}/${NOTES_APP_MONGODB_DATBASE}`;
+
+mongoose.connect(process.env.NOTES_APP_MONGODB_URI, {
         useCreateIndex: true,
         useNewUrlParser: true,
         useFindAndModify: false,
